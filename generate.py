205b16bf895e4900c77ac6dc299cb8146bdc9473
MattIPv4 <me@mattcowley.co.uk>
2019-11-03 00:00:04 +0000

    Script: Generate base MD file

---
+++ generate.py
@@ -0,0 +1,268 @@
+"""
+A script that generates the base MD stats report file, parses the stats Cloudflare provides
+ and exports them to the DB as well as generating an MD table of them for the report.
+"""
+import calendar
+import os
+import re
+import sqlite3
+from random import choice, random
+from typing import List, Dict, Union
+
+import beautifultable
+import requests
+
+
+def generate_regex(order: List[str]) -> str:
+    final = []
+    patterns = {
+        "requests": "(?P<requests>\\d+)",
+        "resources": "(?P<resource>\\S+)",
+        "bandwidth": "(?P<bandwidth>\\d+(?:\\.\\d+)?)"
+    }
+    joiner = "[\\s|│]*"
+    for item in order:
+        if item in patterns:
+            final.append(patterns[item])
+    final = "^" + joiner + joiner.join(final) + joiner + "$"
+    return final
+
+
+def parse_raw(url: str, column_regex: str) -> List[Dict[str, Union[int, float, str]]]:
+    # Get the raw dump from the url
+    raw = requests.get(url).text
+    items = []
+    file_regex = "^cdnjs\\.cloudflare\\.com\\/ajax\\/libs\\/(?P<library>.+?)\\/(?P<version>.+?)\\/(?P<file>.+)$"
+
+    # Iterate over each line of the dump
+    for line in raw.split("\n"):
+        try:
+            # Attempt to find the components through regex
+            x = re.search(column_regex, line)
+            y = x.groupdict()
+
+            # Ensure correct datatypes
+            y["requests"] = int(y["requests"])
+            y["bandwidth"] = float(y["bandwidth"])
+
+            # Parse resource further
+            z = re.search(file_regex, y["resource"]).groupdict()
+            for key, value in z.items():
+                y[key] = value
+
+            items.append(y)
+        except:
+            continue
+    return items
+
+
+def database_data(request_data: List[Dict[str, Union[int, float, str]]], month: int, year: int):
+    # Create db
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+    c.execute(
+        '''CREATE TABLE IF NOT EXISTS DATA (resource text, bandwidth float, requests integer, library text, version text, file text, month integer, year integer)''')
+    conn.commit()
+
+    # Export all the data
+    for row in request_data:
+        c.execute(
+            '''INSERT INTO DATA (resource,bandwidth,requests,library,version,file,month,year) VALUES (?,?,?,?,?,?,?,?)''',
+            [
+                row["resource"], row["bandwidth"], row["requests"], row["library"], row["version"], row["file"], month,
+                year
+            ])
+    conn.commit()
+
+    # Create libraries view
+    c.execute('''CREATE VIEW IF NOT EXISTS libraries AS
+SELECT
+	*,
+	total_requests / days AS requests_per_day,
+	total_bandwidth / days AS bandwidth_per_day
+FROM
+(
+	SELECT
+		SUM(requests) * 100 AS total_requests,
+		SUM(bandwidth) * 100 AS total_bandwidth,
+		library,
+		COUNT(DISTINCT(version || file)) AS files,
+		year,
+		month,
+		(julianday(DATE(year || "-" || printf("%02d", month) || "-01", "+1 month")) - julianday(year || "-" || printf("%02d", month) || "-01")) AS days,
+		year || "-" || printf("%02d", month) as date
+	FROM DATA GROUP BY library, date ORDER BY total_requests DESC
+);''')
+    conn.commit()
+    conn.close()
+
+
+def table_data(request_data: List[Dict[str, Union[int, float, str]]]) -> str:
+    # Create the header
+    header = beautifultable.BeautifulTable(max_width=1000, default_alignment=beautifultable.ALIGN_LEFT)
+    header.set_style(beautifultable.STYLE_MARKDOWN)
+    header.column_headers = ["#", "Requests", "Bandwidth", "cdnjs Resource URL"]
+    header.append_row(["", "", "", ""])
+
+    # Create the base table
+    base_table = beautifultable.BeautifulTable(max_width=1000, default_alignment=beautifultable.ALIGN_LEFT)
+    base_table.set_style(beautifultable.STYLE_MARKDOWN)
+    base_table.detect_numerics = False  # This breaks the formatting
+
+    # Sort the data
+    request_data = sorted(request_data, key=lambda x: x["requests"], reverse=True)
+
+    # Generate the table
+    for i, row in enumerate(request_data):
+        try:
+            base_table.append_row([
+                "{:,}".format(i + 1),
+                "{:,}".format(row["requests"]),
+                "{:,.2f}".format(row["bandwidth"]),
+                "[{0}](https://{0})".format(row["resource"])
+            ])
+        except:
+            continue
+
+    # Export header + table
+    base_table.column_alignments[2] = beautifultable.ALIGN_RIGHT
+    header = str(header)
+    base_table = str(base_table)
+    return header[:header.rfind("\n")] + "\n" + base_table
+
+
+def over_under_nearly(exact: Union[float, int], rounded: int) -> str:
+    return "over" if exact > rounded else ("just under" if rounded - exact <= 0.2 else "nearly")
+
+
+def create_file(table_string: str, month: int, year: int, total_data: dict):
+    variables = {
+        "MONTH": calendar.month_name[month],
+        "YEAR": year,
+        "DAYS": calendar.monthrange(year, month)[1],
+        "TABLE": table_string
+    }
+
+    # Requests
+    REQUESTS_1_PER_TOTAL = total_data["REQUESTS_1_PER"] * 100
+    REQUESTS_3_DAY_TOTAL = (total_data["REQUESTS_3_DAY"] / 3) * variables["DAYS"]
+    REQUESTS = (REQUESTS_1_PER_TOTAL + REQUESTS_3_DAY_TOTAL) / 2
+    variables.update({
+        "REQUESTS_1_PER": "{:,}".format(total_data["REQUESTS_1_PER"]),
+        "REQUESTS_1_PER_TOTAL": "{:,.0f}".format(REQUESTS_1_PER_TOTAL),
+        "REQUESTS_3_DAY": "{:,}".format(total_data["REQUESTS_3_DAY"]),
+        "REQUESTS_3_DAY_TOTAL": "{:,.0f}".format(REQUESTS_3_DAY_TOTAL),
+        "REQUESTS": "{:,.0f}".format(REQUESTS),
+        "REQUESTS_STAT": "",
+        "REQUESTS_HIGHLIGHT": "",  # Key Highlights
+        "REQUESTS_DESCRIPTION": ""  # Total Number of Requests
+    })
+
+    # Requests insights
+    requests_billion = REQUESTS / 1000000000
+    requests_billion_rounded = round(requests_billion)
+    variables["REQUESTS_STAT"] = over_under_nearly(requests_billion, requests_billion_rounded) \
+                                 + " {:,.0f} billion requests".format(requests_billion_rounded)
+    state = bool(round(random()))
+    variables["REQUESTS_HIGHLIGHT"] = ("This month ({} {}), ".format(variables["MONTH"], year) if state else "") \
+                                      + "cdnjs served **{}**".format(variables["REQUESTS_STAT"]) \
+                                      + (" in {} {}.".format(variables["MONTH"], year) if not state else ".")
+    variables["REQUESTS_DESCRIPTION"] = variables["REQUESTS_STAT"].capitalize() \
+                                        + " or {} {:,.1f} billion requests {} day of {}".format(
+        choice(["around", "roughly", "approximately"]),
+        requests_billion / variables["DAYS"],
+        choice(["every", "every single", "each"]), variables["MONTH"])
+
+    # Bandwidth
+    BANDWIDTH_1_PER_TOTAL_GB = total_data["BANDWIDTH_1_PER"] * 100
+    BANDWIDTH_1_PER_TOTAL_PB = BANDWIDTH_1_PER_TOTAL_GB / 1000000
+    BANDWIDTH_3_DAY_TOTAL_GB = (total_data["BANDWIDTH_3_DAY"] / 3) * variables["DAYS"]
+    BANDWIDTH_3_DAY_TOTAL_PB = BANDWIDTH_3_DAY_TOTAL_GB / 1000000
+    BANDWIDTH_GB = (BANDWIDTH_1_PER_TOTAL_GB + BANDWIDTH_3_DAY_TOTAL_GB) / 2
+    BANDWIDTH_PB = BANDWIDTH_GB / 1000000
+    variables.update({
+        "BANDWIDTH_1_PER": "{:,}".format(total_data["BANDWIDTH_1_PER"]),
+        "BANDWIDTH_1_PER_TOTAL_GB": "{:,.1f}".format(BANDWIDTH_1_PER_TOTAL_GB),
+        "BANDWIDTH_1_PER_TOTAL_PB": "{:,.2f}".format(BANDWIDTH_1_PER_TOTAL_PB),
+        "BANDWIDTH_3_DAY": "{:,}".format(total_data["BANDWIDTH_3_DAY"]),
+        "BANDWIDTH_3_DAY_TOTAL_GB": "{:,.1f}".format(BANDWIDTH_3_DAY_TOTAL_GB),
+        "BANDWIDTH_3_DAY_TOTAL_PB": "{:,.2f}".format(BANDWIDTH_3_DAY_TOTAL_PB),
+        "BANDWIDTH_GB": "{:,.1f}".format(BANDWIDTH_GB),
+        "BANDWIDTH_PB": "{:,.2f}".format(BANDWIDTH_PB),
+        "BANDWIDTH_STAT": "",
+        "BANDWIDTH_HIGHLIGHT": "",  # Key Highlights
+        "BANDWIDTH_DESCRIPTION": ""  # Total Bandwidth Usage
+    })
+
+    # Bandwidth insights
+    variables["BANDWIDTH_STAT"] = "a {} consumption of {} PB of data".format(
+        choice(["massive", "huge"]), variables["BANDWIDTH_PB"])
+    state = not state
+    variables["BANDWIDTH_HIGHLIGHT"] = ("This month, " if state else "") \
+                                       + "cdnjs used **{}** to serve these requests" \
+                                           .format(variables["BANDWIDTH_STAT"]) \
+                                       + (" this month." if not state else ".")
+    variables["BANDWIDTH_DESCRIPTION"] = "This gives cdnjs {} for requests {}".format(
+        variables["BANDWIDTH_STAT"].replace("PB", "petabytes").replace("consumption", "bandwidth consumption"),
+        choice(["this month", "in {}".format(variables["MONTH"])]))
+
+    # Per day key highlight
+    bandwidth_days_tb = (BANDWIDTH_GB / variables["DAYS"]) / 1000
+    bandwidth_days_tb_rounded = round(bandwidth_days_tb)
+    requests_days_billion = (REQUESTS / variables["DAYS"]) / 1000000000
+    state = bool(round(random()))
+    variables["PER_DAY_HIGHLIGHT"] = "{} **{} {:,} {} of data and {:,.1f} billion requests {} day** (averaged)." \
+        .format(
+        "That's" if state else "That works out to",
+        over_under_nearly(bandwidth_days_tb, bandwidth_days_tb_rounded),
+        bandwidth_days_tb_rounded,
+        "terabytes" if state else "TB",
+        requests_days_billion,
+        choice(["each", "every"])
+    )
+
+    # Per request key highlight
+    bandwidth_requests_kb = (BANDWIDTH_GB / REQUESTS) * 1000000
+    state = bool(round(random()))
+    variables["PER_REQUEST_HIGHLIGHT"] = "In {}, **each request to cdnjs{} used only {:,.2f} KB of data{}**." \
+        .format(
+        variables["MONTH"],
+        " (on average)" if state else "",
+        bandwidth_requests_kb,
+        "" if state else " on average",
+    )
+
+    # Load in template file
+    with open("template.md") as f:
+        template = f.read()
+
+    # Substitute in variables
+    for key, value in variables.items():
+        template = template.replace("{{" + key + "}}", str(value))
+
+    # Save to new file
+    os.makedirs("./{}".format(year), exist_ok=True)
+    with open("./{}/cdnjs_{}_{}.md".format(year, variables["MONTH"], year), "w+") as f:
+        f.write(template)
+
+
+if __name__ == "__main__":
+    REQUESTS_1_PER = 1500000000
+    REQUESTS_3_DAY = 15000000000
+    BANDWIDTH_1_PER = 30000.00  # GB
+    BANDWIDTH_3_DAY = 300000.00  # GB
+    RAW_TABLE_DATA_URL = ""  # 1% data
+    RAW_TABLE_DATA_ORDER = ["requests", "resources", "bandwidth"]
+    MONTH = 1
+    YEAR = 2019
+
+    regex = generate_regex(RAW_TABLE_DATA_ORDER)
+    items = parse_raw(RAW_TABLE_DATA_URL, regex)
+    database_data(items, MONTH, YEAR)
+    request_table = table_data(items)
+    create_file(request_table, MONTH, YEAR, {
+        "REQUESTS_1_PER": REQUESTS_1_PER,
+        "REQUESTS_3_DAY": REQUESTS_3_DAY,
+        "BANDWIDTH_1_PER": BANDWIDTH_1_PER,
+        "BANDWIDTH_3_DAY": BANDWIDTH_3_DAY
+    })
---
+++ template.md
@@ -0,0 +1,55 @@
+# cdnjs {{MONTH}} {{YEAR}} Usage Stats
+Information provided directly by Cloudflare for the `cdnjs.cloudflare.com` domain. ⛅️
+
+## Key Highlights
+ - {{REQUESTS_HIGHLIGHT}} 🖥
+ - {{BANDWIDTH_HIGHLIGHT}} 📤
+ - {{PER_DAY_HIGHLIGHT}} 🤯
+ - {{PER_REQUEST_HIGHLIGHT}} 🔍
+ 
+### TODO: Library Highlights
+ - 1
+ - 2
+ - 3
+
+## Total Number of Requests
+> The first important stat that we are given is the total number of requests sent to cdnjs.cloudflare.com.
+> 
+> Cloudflare provides this number to us at a 1% sample for the whole month, giving {{REQUESTS_1_PER}} at 1%.\
+> This is {{REQUESTS_1_PER_TOTAL}} when multiplied up to 100%.
+> 
+> We are also given a number of requests for 3 days at a 100% sample, which is {{REQUESTS_3_DAY}}.\
+> This is {{REQUESTS_3_DAY_TOTAL}} when recalculated for the {{DAYS}} days of {{MONTH}}.
+
+To provide the best possible estimate for the entire month, an average of both numbers will be used to generate the
+ estimate for the final number of requests for the month.\
+This results in cdnjs serving approximately {{REQUESTS}} requests in {{MONTH}}.
+
+**{{REQUESTS_DESCRIPTION}}**. 📈\
+TODO: Insight against previous months
+
+## Total Bandwidth Usage
+> Another great stat that Cloudflare has given us again is the bandwidth usage for the cdnjs.cloudflare.com domain.
+> 
+> This number, like total requests, is provided at a 1% sample for the month and in gigabytes: {{BANDWIDTH_1_PER}} GB.\
+> This is {{BANDWIDTH_1_PER_TOTAL_GB}} GB or {{BANDWIDTH_1_PER_TOTAL_PB}} PB when multiplied up to be 100%.
+> 
+> Additionally, a 3 day 100% sample is given by Cloudflare at {{BANDWIDTH_3_DAY}} GB.\
+> This results in {{BANDWIDTH_3_DAY_TOTAL_GB}} GB or {{BANDWIDTH_3_DAY_TOTAL_PB}} PB for the month.
+
+As with the total number of requests and due to the significant difference between the two, both numbers will be used to
+ calculate an average for the final estimate of bandwidth consumed this month.\
+This produces the estimate of {{BANDWIDTH_GB}} GB of bandwidth used for this month by cdnjs.
+
+**{{BANDWIDTH_DESCRIPTION}}**. 🤯\
+TODO: Insight against previous months
+
+## Top 100 Requested Resources
+> These are provided at a 1% sample for the whole of {{MONTH}}.
+> Bandwidth is measured in gigabytes.
+> This data, as well as previous months' data, is available in the SQLite data.db file.
+
+{{TABLE}}
+
+Author: [Matt (IPv4) Cowley](https://mattcowley.co.uk) - If there are any errors, please let me know and I will
+ endeavour to correct them.
